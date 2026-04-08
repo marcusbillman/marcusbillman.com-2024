64417b58f19004ea8ceadee196eefdf351a723d8
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2026-04-08 19:39:29 +0200

    Add support for `forceDark` in DotGrid

--- src/components/DotGrid.tsx
+++ src/components/DotGrid.tsx
@@ -3,18 +3,25 @@ import { twJoin } from 'tailwind-merge';
 interface Props {
   /** Optionally add a dimmed overlay to make the dot grid more subtle */
   dim?: 'default' | 'strong' | 'none';
+  /** Make the component always look like dark theme instead of using dynamic colour tokens */
+  forceDark?: boolean;
 }
 
 /**
  * Dotted grid background. Absolutely positioned to fill its parent. Set `isolation: isolate` on the parent to place at the back.
  */
-export default function DotGrid({ dim = 'none' }: Props) {
+export default function DotGrid({ dim = 'none', forceDark = false }: Props) {
   return (
     <div
       className={twJoin(
-        "absolute inset-0 -z-10 bg-[url('/images/dot-tile-light.svg')] bg-center dark:bg-[url('/images/dot-tile-dark.svg')]",
-        dim === 'default' && 'bg-overlay-dim-default',
-        dim === 'strong' && 'bg-overlay-dim-strong',
+        'absolute inset-0 -z-10 bg-center',
+        forceDark
+          ? 'bg-[url(/images/dot-tile-dark.svg)]'
+          : 'bg-[url(/images/dot-tile-light.svg)] dark:bg-[url(/images/dot-tile-dark.svg)]',
+        dim === 'default' &&
+          (forceDark ? 'bg-black/60' : 'bg-overlay-dim-default'),
+        dim === 'strong' &&
+          (forceDark ? 'bg-black/80' : 'bg-overlay-dim-strong'),
       )}
     ></div>
   );
